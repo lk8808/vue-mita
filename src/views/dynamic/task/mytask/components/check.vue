600e47b55098e4aeb313b5f9383659570eceaf3a
lk8808 <1130493128@qq.com>
2021-02-26 11:04:57 +0800

    20210226

---
+++ src/views/dynamic/task/mytask/components/check.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    处理
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      task: {
+        required: true,
+        type: Object
+      }
+    },
+    data() {
+
+    },
+    watch: {
+      task(val) {
+        
+      }
+    },
+    created() {
+    },
+    methods: {
+      init() {
+        if (this.bizdata.province && this.bizdata.city && this.bizdata.country) {
+          this.bizdata.region = [this.bizdata.province, this.bizdata.city, this.bizdata.country]
+        }
+      },
+      save() {
+        
+      }
+    }
+  }
+</script>
